1531356e9ed2031a1bdc0cda002bd5b71c36334e
Gyuill Kim <rlarbdlf222@gmail.com>
2025-04-17 13:59:14 +0900

    [feat] user, db setting

---
+++ src/main/java/com/example/springJWT/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.springJWT.repository;
+
+import com.example.springJWT.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
